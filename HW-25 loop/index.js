478d581ba011726424d091b8709d8ca421ef8f06
mdshavoahmadejoydev <mdshavoahmadejoydev@gmail.com>
2026-06-19 12:17:37 +0600

    class 25 loop

---
+++ HW-25 loop/index.js
@@ -0,0 +1,46 @@
+// 17 ghor er namota , use by "for loop"
+
+let terget = 17;
+let i = 0;
+// for(i = 1; i<=10; i++) {
+//   console.log(terget + " x " + i + " = " + i*terget);
+// }
+
+
+// ====================
+// use by "while loop"
+
+// i=1;
+// while(i<=10) {
+//   console.log(terget + " x " + i + " = " + i*terget);
+//   i++;
+// }
+
+
+// ====================
+// use by "while loop"
+
+// i=1;
+// do{
+  //   console.log(terget + " x " + i + " = " + i*terget);
+  //   i++;
+  // } while (i<=10);
+  
+  
+  // ====================
+  // use by "for loop" of break & continue condicion
+
+  // for(i=0; i<10; i++) {
+  //   if(i == 5) {
+  //     break;
+  //   }
+  //   console.log(i);
+  // }
+  
+  
+  // for(i=0; i<10; i++) {
+  //   if(i == 5) {
+  //     continue;
+  //   }
+  //   console.log(i);
+  // }
