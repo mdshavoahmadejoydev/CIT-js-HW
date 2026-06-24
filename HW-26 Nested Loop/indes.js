129b5a7448615b43aba05d01b38749de82ab13f2
mdshavoahmadejoydev <mdshavoahmadejoydev@gmail.com>
2026-06-24 23:54:13 +0600

    HW-26, nested loop done

--- HW-26 Nested Loop/indes.js
+++ HW-26 Nested Loop/indes.js
@@ -1,10 +1,24 @@
-let m = prompt("Enter your namota start: ");
-let n = prompt("Enter your namota end: ");
 
-for(let i = m; i <= n; i++) {
-  console.log("=======", i, "er ghor er namta start");
-  for(j = 1; j <= 10; j++) {
-    console.log(i+" X "+j+" = "+i*j)
+let m = Number(prompt("Enter your namota start: "));
+let n = Number(prompt("Enter your namota end: "));
+let rejectNum = Number(prompt("Enter your reject number-"));
+
+for (let i = m; i <= n; i++) {
+  if (i != rejectNum) {
+    console.log("=======", i, "er namta start =========");
+    for (j = 1; j <= 10; j++) {
+      if(i*j != 30) {
+        if(i*j != 40) {
+          if (i*j != 50) {
+            console.log(i + " X " + j + " = " + i * j);
+          }
+        }
+      }
+    }
+    if (i < n) {
+      console.log("=======", i, "er namta end ========");
+    } else {
+      console.log("======= All namta end ========");
+    }
   }
-  console.log("=======", i, "er ghor er namta ");
 }
